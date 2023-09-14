92a2586b9d8fe756aaa2d3b5225dfd1d961252a8
Phong <phonghqm@gmail.com>
2023-09-15 01:09:27 +0700

    export images

---
+++ src/utils/exportImages/index.js
@@ -0,0 +1,25 @@
+import logo_facebook from "../../assets/images/footer/logo-facebook.png";
+import logo_linkedin from "../../assets/images/footer/logo-linkedin.png";
+import logo_twitter from "../../assets/images/footer/logo-twitter.png";
+import logo_youtube from "../../assets/images/footer/logo-youtube.png";
+import logo_gameloft from "../../assets/images/footer/Gameloft_Logo_Flat_White_Line.png";
+import logo_gameloft_black from "../../assets/images/header/gameloft-logo.svg";
+import bg_img from "../../assets/images/hotspotBanner/bg-img.png";
+import icon_star from "../../assets/icons/star.svg";
+import icon_starYellow from "../../assets/icons/starYellow.svg";
+import btn_menu from "../../assets/icons/btn-menu.svg";
+import img_minions from "../../assets/images/registerForm/minions.png";
+
+export {
+  logo_facebook,
+  logo_linkedin,
+  logo_twitter,
+  logo_youtube,
+  logo_gameloft,
+  logo_gameloft_black,
+  icon_star,
+  icon_starYellow,
+  bg_img,
+  btn_menu,
+  img_minions,
+};
