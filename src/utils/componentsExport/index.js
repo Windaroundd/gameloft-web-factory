844e8da069981678dff27a7100be331abf7d54f6
Phong <phonghqm@gmail.com>
2023-09-15 01:03:39 +0700

    export components

---
+++ src/utils/componentsExport/index.js
@@ -0,0 +1,16 @@
+import Header from "../../components/header/Header";
+import HotspotBanner from "../../components/hotspotBanner/HotspotBanner";
+import RegisterForm from "../../components/registerForm/RegisterForm";
+import Community from "../../components/community/Community";
+import EventsPR from "../../components/eventsPR/EventsPR";
+import ExclusiveContent from "../../components/exclusiveContent/ExclusiveContent";
+import Footer from "../../components/footer/Footer";
+
+export {
+  Header,
+  HotspotBanner,
+  RegisterForm,
+  Community,
+  EventsPR,
+  ExclusiveContent,Footer
+};
