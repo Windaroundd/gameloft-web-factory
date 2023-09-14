1d7c031265db471d8a81f1025115191a16f4fbfa
Phong <phonghqm@gmail.com>
2023-09-15 00:50:16 +0700

    create folder structure

---
+++ src/components/eventsPR/EventsPR.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EventsPR = () => {
+  return <div>EventsPR</div>;
+};
+
+export default EventsPR;
---
+++ src/components/hotspotBanner/HotspotBanner.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HotspotBanner = () => {
+  return <div>HotspotBanner</div>;
+};
+
+export default HotspotBanner;
