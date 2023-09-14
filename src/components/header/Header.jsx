7dd344d76b981e6b0ea134e2c394bba1f5d9d959
Phong <phonghqm@gmail.com>
2023-09-15 01:09:40 +0700

    create header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
+import { logo_gameloft_black, btn_menu } from "../../utils/exportImages";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="bg-lightBlue fixed left-0 top-0 z-40 h-20 w-full">
+      <nav className=" mx-auto flex w-[95%] items-center justify-between">
+        <section>
+          <h1>
+            {" "}
+            <NavLink to="https://www.gameloft.com/blog/players">
+              <img src={logo_gameloft_black} alt="logo" />
+            </NavLink>
+          </h1>
+        </section>
+        <section>
+          <button>
+            <img src={btn_menu} alt="menu-btn" />
+          </button>
+        </section>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
