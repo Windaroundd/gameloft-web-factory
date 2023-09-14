f63fd1abc858a459b254402fb7766da7e4121e87
Phong <phonghqm@gmail.com>
2023-09-15 01:06:58 +0700

    config tailwindcss font-family, colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: "Montserrat",
+        RobotoCondensed: "Roboto Condensed",
+      },
+      colors: {
+        lightBlue: "#A8D6FD",
+        mainBlue: "#2699FB",
+        lighterBlue: "#D8EDFF",
+        lightestBlue: "#EBF5FF",
+      },
+    },
   },
   plugins: [],
 };
